ebc67cbd522a4fdea713653ed8e24573463eedaa
Martin Smith <martin@mbs3.org>
2006-08-28 11:51:26 +0000

    Fixed plaintext demo to show correct, localized information.

--- org/mbs/java/swing/widgets/datepicker/CalendarDemo.java
+++ org/mbs/java/swing/widgets/datepicker/CalendarDemo.java
@@ -22,56 +22,40 @@ public class CalendarDemo {
 	public static void main(String[] args) {
 		GregorianCalendar calendar = new GregorianCalendar();
 		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.MONTH, Calendar.APRIL);
+		calendar.setFirstDayOfWeek(Calendar.WEDNESDAY);
 		
-
-		System.out.println("ERA: " + calendar.get(Calendar.ERA));
-		System.out.println("YEAR: " + (calendar.get(Calendar.YEAR)));
-		System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
-		System.out.println("WEEK_OF_YEAR: "
-				+ calendar.get(Calendar.WEEK_OF_YEAR));
-		System.out.println("WEEK_OF_MONTH: "
-				+ calendar.get(Calendar.WEEK_OF_MONTH));
-		System.out.println("Actual MAX WEEK_OF_MONTH: "
-				+ calendar.getActualMaximum(Calendar.WEEK_OF_MONTH));
-
-		System.out.println("DATE: " + calendar.get(Calendar.DATE));
-		System.out.println("DAY_OF_MONTH: "
-				+ calendar.get(Calendar.DAY_OF_MONTH));
-		System.out
-				.println("DAY_OF_YEAR: " + calendar.get(Calendar.DAY_OF_YEAR));
-		System.out.println("DAY_OF_WEEK: " + calendar.get(Calendar.DAY_OF_WEEK));
-		System.out.println("DAY_OF_WEEK_IN_MONTH: "
-				+ calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH));
-		System.out.println("AM_PM: " + calendar.get(Calendar.AM_PM));
-		System.out.println("HOUR: " + calendar.get(Calendar.HOUR));
-		System.out
-				.println("HOUR_OF_DAY: " + calendar.get(Calendar.HOUR_OF_DAY));
-		System.out.println("MINUTE: " + calendar.get(Calendar.MINUTE));
-		System.out.println("SECOND: " + calendar.get(Calendar.SECOND));
-		System.out
-				.println("MILLISECOND: " + calendar.get(Calendar.MILLISECOND));
-		System.out.println("ZONE_OFFSET: "
-				+ (calendar.get(Calendar.ZONE_OFFSET) / (60 * 60 * 1000)));
-		System.out.println("DST_OFFSET: "
-				+ (calendar.get(Calendar.DST_OFFSET) / (60 * 60 * 1000)));
-
-		int month = calendar.get(Calendar.MONTH);
-
-		System.out.println("\tSU\tMO\tTU\tWE\tTH\tFR\tSA");
+		calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+		for(int i = 0; i < 7; i++)
+		{
+			System.out.print("\t" + String.format("%1$ta", calendar));
+			calendar.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		System.out.println(); 
+		
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		while(calendar.get(Calendar.DAY_OF_WEEK) != calendar.getFirstDayOfWeek())
+			calendar.add(Calendar.DAY_OF_YEAR, -1);
 
 		// skip print up to this month
-		for (int i = 1; i < calendar.get(Calendar.DAY_OF_WEEK); i++) {
+		while(calendar.get(Calendar.DAY_OF_MONTH) != calendar.getMinimum(Calendar.DAY_OF_MONTH)) {
 			System.out.print("\tx");
+			calendar.add(Calendar.DAY_OF_YEAR, 1);
 		}
 
+		int month = calendar.get(Calendar.MONTH);
 		while (month == calendar.get(Calendar.MONTH)) {
 			System.out.print("\t" + calendar.get(Calendar.DAY_OF_MONTH));
 			calendar.add(Calendar.DAY_OF_YEAR, 1);
 			if (calendar.get(Calendar.DAY_OF_WEEK) == calendar.getFirstDayOfWeek())
 				System.out.println();
 		}
+		
+		while(calendar.get(Calendar.DAY_OF_WEEK) != calendar.getFirstDayOfWeek()) {
+			System.out.print("\tx");
+			calendar.add(Calendar.DAY_OF_YEAR, 1);
+		}
 		System.out.println();
+
 	}
 
 }
